56d5ab3627b6917332c26c28291f3aa76dc35c2e
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-10-31 16:04:30 -0400

    subform buttons now display forms

--- public/scripts/showForm.js
+++ public/scripts/showForm.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const forms = document.querySelectorAll('.subForm')
     const showFormButtons = document.querySelectorAll('.showSubForm')
+    const func = (e) => {
+        e.preventDefault()
+        const buttonFormType = e.target.dataset.form
+        forms.forEach(form => {
+            form.dataset.form === buttonFormType ? form.style.display = 'block' : form.style.display = 'none'
+        })
+    }
+    showFormButtons.forEach((button) => {
+        button.addEventListener('click', (e) => func(e))
+    })
 });
